fdf8f871f02bef848a803da60a7ef4721f90d17b
Thamires Lopes <thamiresl686@gmail.com>
2021-04-17 14:14:22 -0300

    Function to calculate age

--- src/components/appointment/index.jsx
+++ src/components/appointment/index.jsx
@@ -1,10 +1,35 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-unused-vars */
 import React, { useContext } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { VaccineContext } from '../../VaccineContext';
 
 const formatDate = (date) => new Date(date).toLocaleDateString();
+
 const formatTime = (date) => `${new Date(date).getHours()}:00`;
+
+const calculateAge = (date) => {
+  const today = new Date();
+  const todayYear = today.getFullYear();
+  const todayMonth = today.getMonth() + 1;
+  const todayDay = today.getDate();
+
+  const birthdate = new Date(date);
+  const birthdateYear = birthdate.getFullYear();
+  const birthdateMonth = birthdate.getMonth() + 1;
+  const birthdateDay = birthdate.getDate();
+
+  let result = todayYear - birthdateYear;
+  if (todayMonth === birthdateMonth) {
+    if (todayDay < birthdateDay) {
+      result -= 1;
+    }
+  } else if (todayMonth < birthdateMonth) {
+    result -= 1;
+  }
+
+  return result;
+};
 
 const Appointment = () => {
   const [vaccines, setVaccines] = useContext(VaccineContext);
@@ -12,12 +37,15 @@ const Appointment = () => {
     <div>
       {
         vaccines.length ? vaccines.map((vaccine, index) => (
-          <Card>
+          <Card key={vaccine._id}>
             <Card.Header as="h5">{`Vacina: ${index}`}</Card.Header>
             <Card.Body>
               <Card.Title>{`${vaccine.firstName} ${vaccine.lastName}`}</Card.Title>
               <Card.Text>
                 {`Aniversário: ${formatDate(vaccine.birthday)}`}
+              </Card.Text>
+              <Card.Text>
+                {`Idade: ${calculateAge(vaccine.birthday)}`}
               </Card.Text>
               <Card.Text>
                 {`Data da vacina: ${formatDate(vaccine.vaccineDay)}`}
