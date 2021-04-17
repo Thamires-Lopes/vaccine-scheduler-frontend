e2b102215bcfb7fed78ab825b34e74ac043f20de
Thamires Lopes <thamiresl686@gmail.com>
2021-04-17 10:24:23 -0300

    Basic structure of appointment component done

---
+++ src/components/appointment/index.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { VaccineContext } from '../../VaccineContext';
+
+const Appointment = () => {
+  const [vaccines, setVaccines] = useContext(VaccineContext);
+  return (
+    <div>
+      {
+        vaccines.length ? vaccines.map((vaccine, index) => (
+          <Card>
+            <Card.Header as="h5">{`Vacina: ${index}`}</Card.Header>
+            <Card.Body>
+              <Card.Title>{`${vaccine.firstName} ${vaccine.lastName}`}</Card.Title>
+              <Card.Text>
+                {`Aniversário: ${vaccine.birthday}`}
+              </Card.Text>
+              <Card.Text>
+                {`Data da vacina: ${vaccine.vaccineDay}`}
+              </Card.Text>
+              <Card.Text>
+                {`Horário da vacina: ${vaccine.vaccineTime}`}
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        )) : (
+          <div>Sem dados</div>
+        )
+      }
+    </div>
+  );
+};
+
+export default Appointment;
