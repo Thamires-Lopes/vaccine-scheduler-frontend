16bfba16f80590c36f87d17df4656d5b3193ebac
Thamires Lopes <thamiresl686@gmail.com>
2021-04-21 12:41:10 -0300

    Saving form content in session storage

--- src/components/registerForm/index.jsx
+++ src/components/registerForm/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import {
   Form, Button,
 } from 'react-bootstrap';
 import { toast } from 'react-toastify';
+import { parseISO } from 'date-fns';
 import axios from '../../utils/api';
 import FormDatePicker from './formDatePicker';
 import FormTimePicker from './formTimePicker';
@@ -32,88 +33,127 @@ const onSubmit = async (values) => {
   }
 };
 
-const RegisterForm = () => (
-  <Formik
-    validationSchema={schema}
-    onSubmit={onSubmit}
-    initialValues={{
-      firstName: '',
-      lastName: '',
-      birthday: null,
-      vaccineDay: null,
-      vaccineTime: null,
-    }}
-  >
-    {({
-      handleSubmit,
-      handleChange,
-      values,
-      touched,
-      isValid,
-      errors,
-      setFieldValue,
-    }) => (
-      <div>
-        <Form className="m-4" noValidate>
-          <Form.Group>
-            <Form.Label>Primeiro nome</Form.Label>
-            <Form.Control
-              type="text"
-              name="firstName"
-              value={values.firstName}
-              onChange={handleChange}
-              isValid={touched.firstName && !errors.firstName}
-              isInvalid={touched.firstName && !!errors.firstName}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Sobrenome</Form.Label>
-            <Form.Control
-              type="text"
-              name="lastName"
-              value={values.lastName}
-              onChange={handleChange}
-              isValid={touched.lastName && !errors.lastName}
-              isInvalid={touched.lastName && !!errors.lastName}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Data de nascimento</Form.Label>
-            <FormDatePicker
-              name="birthday"
-              value={values.birthday}
-              isValid={!errors.birthday}
-              isInvalid={touched.birthday && !!errors.birthday}
-              onChange={setFieldValue}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Dia da vacina</Form.Label>
-            <FormDatePicker
-              name="vaccineDay"
-              value={values.vaccineDay}
-              isValid={!errors.vaccineDay}
-              isInvalid={touched.vaccineDay && !!errors.vaccineDay}
-              onChange={setFieldValue}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Hora da vacina</Form.Label>
-            <FormTimePicker
-              name="vaccineTime"
-              value={values.vaccineTime}
-              isValid={!errors.vaccineTime}
-              isInvalid={touched.vaccineTime && !!errors.vaccineTime}
-              onChange={setFieldValue}
-            />
-          </Form.Group>
-          <Button disabled={!isValid} onClick={handleSubmit}>
-            Enviar
-          </Button>
-        </Form>
-      </div>
-    )}
-  </Formik>
-);
+const RegisterForm = () => {
+  const [formValues, setFormValues] = useState({});
+
+  const setInitialValues = () => {
+    const initialValues = sessionStorage.getItem('values');
+    if (initialValues) {
+      let form = JSON.parse(initialValues);
+
+      form = {
+        ...form,
+        birthday: parseISO(form.birthday),
+        vaccineDay: parseISO(form.vaccineDay),
+        vaccineTime: parseISO(form.vaccineTime),
+      };
+
+      setFormValues(form);
+    } else {
+      setFormValues({
+        firstName: '',
+        lastName: '',
+        birthday: null,
+        vaccineDay: null,
+        vaccineTime: null,
+      });
+    }
+  };
+
+  useEffect(() => {
+    setInitialValues();
+  }, []);
+
+  return (
+    <Formik
+      enableReinitialize
+      validationSchema={schema}
+      onSubmit={onSubmit}
+      initialValues={formValues}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        values,
+        touched,
+        isValid,
+        errors,
+        setFieldValue,
+      }) => {
+        const saveFormValues = () => {
+          if (!(Object.keys(values).length === 0 && values.constructor === Object)) {
+            sessionStorage.setItem('values', JSON.stringify(values));
+          }
+        };
+
+        useEffect(() => {
+          saveFormValues();
+        }, [values]);
+
+        return (
+          <div>
+            <Form className="m-4" noValidate>
+              <Form.Group>
+                <Form.Label>Primeiro nome</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="firstName"
+                  value={values.firstName}
+                  onChange={handleChange}
+                  isValid={touched.firstName && !errors.firstName}
+                  isInvalid={touched.firstName && !!errors.firstName}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Sobrenome</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="lastName"
+                  value={values.lastName}
+                  onChange={handleChange}
+                  isValid={touched.lastName && !errors.lastName}
+                  isInvalid={touched.lastName && !!errors.lastName}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Data de nascimento</Form.Label>
+                <FormDatePicker
+                  name="birthday"
+                  value={values.birthday}
+                  isValid={!errors.birthday}
+                  isInvalid={touched.birthday && !!errors.birthday}
+                  onChange={setFieldValue}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Dia da vacina</Form.Label>
+                <FormDatePicker
+                  name="vaccineDay"
+                  value={values.vaccineDay}
+                  isValid={!errors.vaccineDay}
+                  isInvalid={touched.vaccineDay && !!errors.vaccineDay}
+                  onChange={setFieldValue}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Hora da vacina</Form.Label>
+                <FormTimePicker
+                  name="vaccineTime"
+                  value={values.vaccineTime}
+                  isValid={!errors.vaccineTime}
+                  isInvalid={touched.vaccineTime && !!errors.vaccineTime}
+                  onChange={setFieldValue}
+                />
+              </Form.Group>
+              <Button disabled={!isValid} onClick={handleSubmit}>
+                Enviar
+              </Button>
+            </Form>
+          </div>
+        );
+      }}
+    </Formik>
+  );
+};
 
 export default RegisterForm;
