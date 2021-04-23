77a593ffa80d93da58bd0075e3ff352888b35f49
Thamires Lopes <thamiresl686@gmail.com>
2021-04-23 20:38:55 -0300

    Added home page test

---
+++ src/tests/components/pages/home.spec.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from '../../../pages/home/index';
+
+describe('Renders', () => {
+  it('renders home page', () => {
+    const { queryByText } = render(<Home />);
+
+    expect(queryByText('Cadastro')).toBeTruthy();
+    expect(queryByText('Listagem')).toBeTruthy();
+  });
+});
